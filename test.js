c1841e3a0d47de073952eb106a58f051ae129c2a
VithuJey <vithushjeytharma@gmail.com>
2020-03-02 20:38:59 +0530

    Add changes to fully extract using url

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
-"use strict";
+// "use strict";
 
-let date = '/Date(1582934400000)/'
-console.log(Date(date.split('/')[1]))
+// let date = '/Date(1582934400000)/'
+// console.log(Date(date.split('/')[1]))
 
-// let obj = {
-//     "name": "asasas",
-// }
+let obj = {
+  name: "asasas",
+  ty: {
+    ho: {
+      jo: []
+    },
+    to: 5
+  }
+};
 
-// if("name" in obj) console.log("true")
+// to check the availability of a key in a nested JSON
+function getNested(obj, ...args) {
+  return args.reduce((obj, level) => obj && obj[level], obj);
+}
+
+if (getNested(obj, "ty", "to")) console.log("true");
